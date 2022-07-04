be01001b0b7c4c5dbbbfb9a74a9b7b025a13e80f
himank1911 <95181792+himank1911@users.noreply.github.com>
2022-07-04 23:18:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function button1() {
+	const header1 = document.getElementById("l1");
+	header1.textContent = "You've clicked";
+
+	const header2 = document.getElementById("l2");
+	header2.textContent = "Button-1";	
+}
+
+function button2() {
+	const header1 = document.getElementById("l1");
+	header1.textContent = "You've clicked";
+
+	const header2 = document.getElementById("l2");
+	header2.textContent = "Button-2";	
+}
